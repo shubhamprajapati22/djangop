4513f9a76ec55f89b69ecd1b69090bdedd8f5b3f
shubhamprajapati22 <nobishkm@gmail.com>
2020-05-01 23:10:38 +0530

    connectin project with postgresql database

--- firstProject/settings.py
+++ firstProject/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'firstProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',
+        'USER' : 'postgres',
+        'PASSWORD' : '57575088'
+        'HOST' : 'localhost',
     }
 }
 
