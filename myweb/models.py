3d7b5de4c6848577b111431c9f271a0df693344b
shubhamprajapati22 <nobishkm@gmail.com>
2020-05-03 23:50:55 +0530

    implementig forcefully for loop, if statement and a model for my web

--- myweb/models.py
+++ myweb/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class imagess:
+    path_img : str
+    h : bool
--- myweb/views.py
+++ myweb/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from .models import imagess
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    img1 = imagess()
+    img1.path_img = "shubham.JPG"
+    img1.h = False
+    img2 = imagess()
+    img2.path_img = "dipak.jpg"
+    img2.h = True
+    img3 = imagess()
+    img3.path_img = "mammi.jpg"
+    img3.h = True
+
+    imgss = [img1, img2, img3]
+
+    return render(request, 'index.html', {'imgs1' : imgss})
 
 def login(request):
     return render(request, 'login.html')
