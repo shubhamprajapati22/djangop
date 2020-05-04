6c5ead515e151491783a0f84de1a62fd993c2043
shubhamprajapati22 <nobishkm@gmail.com>
2020-05-04 21:45:18 +0530

    creating a table in database

--- myweb/models.py
+++ myweb/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-class imagess:
-    path_img : str
-    h : bool
+class family_d(models.Model):   #for inheriting the feature of database
+    path_img = models.ImageField(upload_to='Fpics')
+    h = models.BooleanField()
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
--- myweb/views.py
+++ myweb/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render
-from .models import imagess
+from .models import family_d
 
 # Create your views here.
 def index(request):
-    img1 = imagess()
+    img1 = family_d()
     img1.path_img = "shubham.JPG"
     img1.h = False
-    img2 = imagess()
+    img1.name = 'Shubham'
+    img1.age = 22
+
+    img2 = family_d()
     img2.path_img = "dipak.jpg"
     img2.h = True
-    img3 = imagess()
+    img2.name = 'dipak'
+    img2.age = 30
+
+    img3 = family_d()
     img3.path_img = "mammi.jpg"
     img3.h = True
+    img3.name = 'Mammi'
+    img3.age = 45
 
     imgss = [img1, img2, img3]
 
